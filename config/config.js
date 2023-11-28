5cb34717d1b7919b97d460e51ca6105c144d231b
SowmyaGopinath <sowmi.2694@gmail.com>
2023-11-28 20:32:48 +0530

    first commit

---
+++ config/config.js
@@ -0,0 +1,6 @@
+const clientURL = "https://urlsht.netlify.app"
+const BASE_URL = "https://short-url-prbk.onrender.com"; // For Short URL
+const DAILY_LIMIT = 10;
+const MONTHLY_LIMIT = 100;
+
+export { clientURL, BASE_URL, DAILY_LIMIT, MONTHLY_LIMIT };
